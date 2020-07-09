2240f3fdad6b5eaa9b453b6cafd50aef4a54f602
thomas schnider <tws6170@gmail.com>
2020-07-09 17:09:23 +0200

    Update app.R

--- app.R
+++ app.R
@@ -4,7 +4,12 @@
 # Thomas Schnider, Prof. Dr. med.
 # Charles Minto, MB ChB
 
-library(shiny)
+list.of.packages <- c("shiny","shinyBS","graphics")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+
+require(shiny)
 require(shinyBS)
 require(graphics)
 
